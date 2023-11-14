559153fa97ed569763009b376ad1b17a19eaf0ad
Jonnathan Ribeiro <jrib_st@outlook.com>
2023-11-14 16:25:01 -0300

    Atualização do SQL.
    Atualiza as tabelas e as operações CRUD: create (criação), read (leitura), update (atualização) e delete (exclusão).

--- Banco de Dados/Modelo_FISICO.sql
+++ Banco de Dados/Modelo_FISICO.sql
@@ -12,7 +12,7 @@ CREATE TABLE Usuarios (
 CREATE TABLE Contatos (
     id_contato INT PRIMARY KEY,
     nome VARCHAR(40),
-    email VARCHAR(80,
+    email VARCHAR(80),
     telefone VARCHAR(15),
     cep VARCHAR(9),
     endereco VARCHAR(100),
@@ -199,3 +199,187 @@ ALTER TABLE Gera ADD CONSTRAINT FK_Gera_2
     FOREIGN KEY (fk_Videos_id_video)
     REFERENCES Videos (id_video)
     ON DELETE SET NULL;
+    
+-- Inserindo dados na tabela Usuarios
+INSERT INTO Usuarios VALUES 
+(default, 'John', 'john@gmail.com', '157'), 
+(default, 'Amanda', 'amanda@gmail.com', '354');
+
+-- Inserindo dados na tabela Contatos
+INSERT INTO Contatos VALUES 
+(default, 'John', 'john@gmail.com', '1234567890', '12345678', 'Rua 1', '10', 'Apto 101', 'Bairro 1', 'SP', 'São Paulo', 'Olá!', 1, NULL),
+(default, 'Amanda', 'amanda@gmail.com', '0987654321', '87654321', 'Rua 2', '20', 'Apto 202', 'Bairro 2', 'DF', 'Brasília', 'Oi!', 2, NULL);
+
+-- Inserindo dados na tabela Voluntarios
+INSERT INTO Voluntarios VALUES 
+(default, 'Disponível aos fins de semana', 'Meio ambiente, educação', 1);
+
+-- Inserindo dados na tabela Doacoes
+INSERT INTO Doacoes VALUES 
+(default, 50.00, CURDATE());
+
+-- Inserindo dados na tabela Campanhas
+INSERT INTO Campanhas VALUES 
+(default, 'Campanha de Arrecadação', 'Arrecadação de fundos para causas sociais', 500.00, CURDATE(), DATE_ADD(CURDATE(), INTERVAL 30 DAY));
+
+-- Inserindo dados na tabela Organizacoes
+INSERT INTO Organizacoes VALUES 
+(default, 'ONG Ajudante', 'Rua Principal, 123', 1);
+
+-- Inserir dados na tabela de Vídeos
+INSERT INTO Videos (id_video, titulo, caminho_arquivo, duracao)
+VALUES 
+    ('a1', 'Diversidade e inclusão', '../vid/Diversidade e inclusão.mp4', '0:12'),
+    ('a2', 'Preparo e Cuidado Com o Solo', '../vid/Preparo e Cuidado Com o Solo.mp4', '1:02'),
+    ('a3', 'Ações Voluntária de Limpeza', '../vid/Ações Voluntária de Limpeza.mp4', '0:14'),
+    ('a4', 'Empoderamento e Liderança', '../vid/Empoderamento e Liderança.mp4', '0:08'),
+    ('a5', 'Permacultura', '../vid/Permacultura.mp4', '0:15'),
+    ('a6', 'Descarte Consciente', '../vid/Descarte Consciente.mp4', '0:09'),
+    ('a7', 'Agricultura Sustentável', '../vid/Agricultura Sustentável.mp4', '0:29'),
+    ('a8', 'Consumo Consciente', '../vid/Consumo Consciente.mp4', '0:04'),
+    ('a9', 'Direitos Humanos e a Diversidade LGBTQIA+', '../vid/Direitos Humanos e a Diversidade LGBTQIA.mp4', '0:17'),
+    ('a10', 'A Importância da Liderança na Promoção dos Direitos Humanos e da Igualdade', '../vid/A Importância da Liderança na Promoção dos Direitos Humanos e da Igualdade.mp4', '0:39');
+
+-- Inserindo dados na tabela Envia_Contatos_Usuarios_Voluntarios
+INSERT INTO Envia_Contatos_Usuarios_Voluntarios VALUES 
+(1, 1, 1),
+(2, 2, 1);
+
+-- Inserindo dados na tabela Registro
+INSERT INTO Registro VALUES 
+(1, 1),
+(2, 1);
+
+-- Inserindo dados na tabela Faz_Usuarios_Voluntarios_Doacoes
+INSERT INTO Faz_Usuarios_Voluntarios_Doacoes VALUES 
+(1, 1, 1),
+(2, 1, 1);
+
+-- Inserindo dados na tabela Coordena
+INSERT INTO Coordena VALUES 
+(1, 1);
+
+-- Inserindo dados na tabela Assiste
+INSERT INTO Assiste VALUES 
+(1, 1),
+(2, 1);
+
+-- Inserindo dados na tabela Pertence
+INSERT INTO Pertence VALUES 
+(1, 1);
+
+-- Inserindo dados na tabela Participa
+INSERT INTO Participa VALUES 
+(1, 1);
+
+-- Inserindo dados na tabela Visualizacoes
+INSERT INTO Visualizacoes VALUES 
+(default, '10 minutos', CURDATE());
+
+-- Inserindo dados na tabela Gera
+INSERT INTO Gera VALUES 
+(1, 1);
+
+-- Selecionando todos os usuários e seus contatos
+SELECT U.*, C.*
+FROM Usuarios U
+JOIN Contatos C ON U.id_usuario = C.id_usuario;
+
+-- Selecionando todos os voluntários e suas doações
+SELECT V.*, D.*
+FROM Voluntarios V
+JOIN Faz_Usuarios_Voluntarios_Doacoes FUV ON V.id_voluntario = FUV.fk_Voluntarios_id_voluntario
+JOIN Doacoes D ON FUV.fk_Doacoes_id_doacao = D.id_doacoes;
+
+-- Obtendo informações sobre as campanhas e organizações associadas
+SELECT C.*, O.*
+FROM Campanhas C
+LEFT JOIN Organizacoes O ON C.id_campanha = O.id_campanha;
+
+-- Listando os vídeos assistidos por usuários
+SELECT U.nome AS Nome_Usuario, V.titulo AS Titulo_Video
+FROM Usuarios U
+JOIN Assiste A ON U.id_usuario = A.fk_Usuarios_id_usuario
+JOIN Videos V ON A.fk_Videos_id_video = V.id_video;
+
+-- Recuperando detalhes das visualizações e vídeos associados
+SELECT V.*, Vi.*
+FROM Videos V
+JOIN Gera G ON V.id_video = G.fk_Videos_id_video
+JOIN Visualizacoes Vi ON G.fk_Visualizacoes_id_visualizacao = Vi.id_visualizacao;
+
+-- Encontrando usuários que participam de campanhas
+SELECT U.*, P.*
+FROM Usuarios U
+JOIN Participa P ON U.id_usuario = P.fk_Usuarios_id_usuario;
+
+-- Obtendo contatos enviados por voluntários
+SELECT C.*
+FROM Contatos C
+JOIN Envia_Contatos_Usuarios_Voluntarios ECUV ON C.id_contato = ECUV.fk_Contatos_id_contato;
+
+-- Verificando quem coordena campanhas
+SELECT V.nome AS Nome_Voluntario, C.nome AS Nome_Campanha
+FROM Voluntarios V
+JOIN Coordena Co ON V.id_voluntario = Co.fk_Voluntarios_id_voluntario
+JOIN Campanhas C ON Co.fk_Campanhas_id_campanha = C.id_campanha;
+
+-- Selecionando todos os vídeos
+SELECT * FROM Videos;
+
+-- Atualizando nome e email de um usuário na tabela Usuarios
+UPDATE Usuarios SET 
+	nome = 'João', 
+	email = 'joao@gmail.com' 
+WHERE id_usuario = 1;
+
+-- Atualizando informações de contato na tabela Contatos
+UPDATE Contatos 
+SET 
+	nome = 'João', 
+	email = 'joao@gmail.com', 
+	telefone = '987654321', 
+	cep = '87654321', 
+	endereco = 'Rua Atualizada', 
+	numero = '25', 
+	complemento = 'Apto 303', 
+	bairro = 'Novo Bairro', 
+	estado = 'RJ', 
+	cidade = 'Rio de Janeiro', 
+	mensagem = 'Mensagem Atualizada'
+WHERE id_usuario = 1;
+
+-- Modificando a disponibilidade e interesses de um voluntário na tabela Voluntarios
+UPDATE Voluntarios SET 
+	disponibilidade = 'Fins de semana e feriados',
+	interesses = 'Meio ambiente, saúde' 
+WHERE id_voluntario = 1;
+
+-- Excluindo um usuário e seu contato associado
+DELETE FROM Contatos WHERE id_usuario = 1;
+DELETE FROM Usuarios WHERE id_usuario = 1;
+
+-- Excluindo um voluntário e suas doações associadas
+DELETE FROM Faz_Usuarios_Voluntarios_Doacoes WHERE fk_Voluntarios_id_voluntario = 1;
+DELETE FROM Doacoes WHERE id_doacoes IN (SELECT fk_Doacoes_id_doacao FROM Faz_Usuarios_Voluntarios_Doacoes WHERE fk_Voluntarios_id_voluntario = 1);
+DELETE FROM Voluntarios WHERE id_voluntario = 1;
+
+-- Excluindo uma campanha e a organização associada (se houver)
+DELETE FROM Organizacoes WHERE id_campanha = 1;
+DELETE FROM Campanhas WHERE id_campanha = 1;
+
+-- Excluindo registros de visualizações e vídeos associados
+DELETE FROM Gera WHERE fk_Visualizacoes_id_visualizacao = 1;
+DELETE FROM Visualizacoes WHERE id_visualizacao = 1;
+
+-- Excluindo um usuário participante de uma campanha
+DELETE FROM Participa WHERE fk_Usuarios_id_usuario = 1;
+
+-- Excluindo registros de envio de contatos por voluntários
+DELETE FROM Envia_Contatos_Usuarios_Voluntarios WHERE fk_Usuarios_id_usuario = 1;
+
+-- Excluindo registros de coordenação de campanhas por voluntários
+DELETE FROM Coordena WHERE fk_Voluntarios_id_voluntario = 1;
+
+-- Excluindo registros de assistência a vídeos por usuários
+DELETE FROM Assiste WHERE fk_Usuarios_id_usuario = 1;
